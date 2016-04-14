ff130fff83366ddd8cd2d764af0894558670d3d6
ER27 <rajmund.nowicki@gmail.com>
2016-04-14 18:13:59 +0200

    Initial commit

---
+++ src/test/java/smokeTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Rajmund Nowicki on 2016-04-14.
+ */
+public class smokeTest {
+
+    @Test
+    public void startWebDriver(){
+        WebDriver driver = new FirefoxDriver();
+        driver.navigate().to("http://www.seleniumsimplified.com");
+
+        Assert.assertTrue("title should start with Selenium Simplified",
+                driver.getTitle().startsWith("Selenium Simplified"));
+
+        driver.close();
+        driver.quit();
+    }
+}
